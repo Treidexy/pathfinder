29addce1fa8e06947b205b23fee39747b81f35f4
Treidex <29633838+Treidexy@users.noreply.github.com>
2024-05-02 20:18:56 -0700

    FUCK ZIG

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,20 @@
 const std = @import("std");
 const rl = @import("raylib");
-const pathfinder = @import("pathfinder.zig");
+const pf = @import("pathfinder.zig");
+
+const width = pf.width;
+const height = pf.height;
+const size = pf.size;
+const thicc = 30;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Hello, world!", .{});
-    defer stdout.print("Goodbye, world!", .{});
+    defer stdout.print("Goodbye, world!", .{}) catch {};
 
-    rl.initWindow(640, 480, "Heigh Heogh!");
+    _ = pf.Board.init();
+
+    rl.initWindow(thicc * pf.width, thicc * pf.height, "Heigh Heogh!");
     defer rl.closeWindow();
 
     rl.setTargetFPS(60);
@@ -16,7 +23,13 @@ pub fn main() !void {
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        rl.clearBackground(rl.Color.white);
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+        rl.clearBackground(rl.Color.init(64, 64, 64, 255));
+
+        for (0..size) |i| {
+            const x: i32 = @as(i32, @intCast(i)) % width;
+            const y: i32 = @as(i32, @intCast(i)) / width;
+
+            rl.drawRectangle(x * thicc, y * thicc, thicc, thicc, rl.Color.green);
+        }
     }
 }
--- src/pathfinder.zig
+++ src/pathfinder.zig
@@ -1,14 +1,50 @@
 const std = @import("std");
-const TileSet = std.AutoHashMap(Square, i32);
-const BitSet = std.bit_set.DynamicBitSet;
+pub const TileMap = std.AutoHashMap(Square, i32);
+pub const Area = std.bit_set.ArrayBitSet(usize, size);
+
+pub var allocator: std.mem.Allocator = undefined;
+pub const width: i32 = 18;
+pub const height: i32 = 32;
+pub const size = width * height;
 
 pub const Board = struct {
-    tiles: TileSet,
-    safes: BitSet,
-    flags: BitSet,
+    tiles: TileMap,
+    safes: Area,
+    flags: Area,
+
+    pub fn init() Board {
+        allocator = std.heap.c_allocator;
+
+        return .{
+            .tiles = TileMap.init(allocator),
+            .safes = Area.initEmpty(),
+            .flags = Area.initEmpty(),
+        };
+    }
 };
 
-pub const Square = struct {
-    x: i32,
-    y: i32,
+pub const Square = i32;
+
+pub const Sector = struct {
+    area: Area,
+    mag: i32,
 };
+
+pub fn adjacent(self: Square, board: Board) !Area {
+    var area = Area.initEmpty();
+
+    for (-1..2) |dy| {
+        for (-1..2) |dx| {
+            const x = self % board.width + dx;
+            const y = self / board.width + dy;
+
+            if (x < 0 or x >= board.width or y < 0 or y >= board.height) {
+                continue;
+            }
+
+            area.set(x + y * board.width);
+        }
+    }
+
+    return area;
+}
